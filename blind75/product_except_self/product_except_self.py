6c19a1c16ab90af9ea7a7f9c96dbb468e62f3feb
deblina <deblina@uchicago.edu>
2024-09-28 23:55:00 -0400

    clean but wip

---
+++ blind75/product_except_self/product_except_self.py
@@ -0,0 +1,9 @@
+def productExceptSelf(nums): 
+    rv = []
+    for i in nums: 
+        
+
+
+if __name__ == '__main__': 
+    assert productExceptSelf([1,2,3,4]) == [24,12,8,6]
+    assert productExceptSelf([-1,1,0,-3,3]) == [0,0,9,0,0]
