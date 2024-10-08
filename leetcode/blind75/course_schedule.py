238c478736ea3c7d42554af6fb1aec3bc7f4fb42
deblina <deblina@uchicago.edu>
2024-10-07 23:07:57 -0400

    don't get but its working

--- leetcode/blind75/course_schedule.py
+++ leetcode/blind75/course_schedule.py
@@ -1,7 +1,28 @@
 from typing import List 
-def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-    raise NotImplemented
+def canFinish(numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+    adjacency_list = {i: [] for i in range(numCourses)}
+    for pair in prerequisites:
+        adjacency_list[pair[0]].append(pair[1])
+    
+    visiting = set()
+    def dfs(course):
+        if course in visiting: 
+            return False 
+        if adjacency_list[course] == []: 
+            return True 
+        visiting.add(course)
+        for pre_req in adjacency_list[course]: 
+            if not dfs(pre_req): return False 
+        visiting.remove(course)
+        adjacency_list[course] = []
+        return True 
+    
+    for course in range(numCourses): 
+        if not dfs(course): return False 
+    return True 
+
 
 
 assert canFinish(2, [[1,0]]) == True 
-assert canFinish(2, [[1,0], [0,1]])
+assert canFinish(2, [[1,0], [0,1]]) == False
