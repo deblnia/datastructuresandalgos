8e8b5300f6260672bb4b2586c0f29143182c3581
deblina <deblina@uchicago.edu>
2024-10-07 21:31:10 -0400

    folders

---
+++ leetcode/blind75/course_schedule.py
@@ -0,0 +1,7 @@
+from typing import List 
+def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+    raise NotImplemented
+
+
+assert canFinish(2, [[1,0]]) == True 
+assert canFinish(2, [[1,0], [0,1]])
