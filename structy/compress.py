a8e2300a6328722a77607f247544ea196c62aa13
deblina <deblina@uchicago.edu>
2024-10-08 18:35:13 -0400

    o(n) solution)

--- structy/compress.py
+++ structy/compress.py
@@ -1,18 +1,42 @@
 def compress(s):
-  s += '!'
-  i = 0 
-  j = 0 
-  rv = ''
-  while j < len(s):
-    if s[i] == s[j]: 
-      j += 1 
-    else: 
-      num = str(j-i)
-      if num == '1': 
-        rv += (s[i])
-      else: rv += (num + s[i])
-      i = j 
-  return rv 
+
+#   this is technically O(n^2) since we run through the while loop n times 
+#   and strings are immutable in python 
+
+#   s += '!'
+#   i = 0 
+#   j = 0 
+#   rv = ''
+#   while j < len(s):
+#     if s[i] == s[j]: 
+#       j += 1 
+#     else: 
+#       num = str(j-i)
+#       if num == '1': 
+#         rv += (s[i])
+#       else: rv += (num + s[i])
+#       i = j 
+#   return rv 
+
+#   this version is o(n) since we use a list 
+
+    s += '!'
+    i = 0 
+    j = 0 
+    rv = []
+    while j < len(s): 
+        if s[i] == s[j]:
+            j += 1 
+        else: 
+            num = str(j-i)
+            if num == '1': 
+                rv.append(s[i])
+            else: 
+                rv.append(num)
+                rv.append(s[i])
+            i = j 
+    return ''.join(rv)
+
 
 
 assert compress('ccaaatsss') == '2c3at3s'
