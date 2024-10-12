b20d8c08b3a7098c5f83cb615c0b9c75b784f5eb
deblina <deblina@uchicago.edu>
2024-10-11 21:16:53 -0400

    finished

--- structy/intersection.py
+++ structy/intersection.py
@@ -11,6 +11,17 @@ def intersection(a, b):
     #     if i in j:
     #         ans.append(i)
     # return ans 
+    # ans = []
+    # setA = set(a)
+    # for i in b: 
+    #     if i in setA:
+    #         ans.append(b)
+    # return ans 
+
+    ans = []
+    setA = set(a)
+    return sorted([item for item in b if item in setA])
+
 
 
 
