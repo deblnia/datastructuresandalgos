3cd69469fba6275dba1c2bf4cb6ba19bbb26ca24
deblina <deblina@uchicago.edu>
2024-10-11 21:14:10 -0400

    intersection

---
+++ structy/intersection.py
@@ -0,0 +1,24 @@
+def intersection(a, b): 
+    # ans = []
+    # for i in a: 
+    #     for j in b: 
+    #         if i == j: 
+    #             ans.append(i)
+    #             break
+    # return sorted(ans)
+    # ans = []
+    # for i in a:
+    #     if i in j:
+    #         ans.append(i)
+    # return ans 
+
+
+
+assert intersection([4,2,1,6], [3,6,9,2,10]) == [2,6]
+assert intersection([2,4,6], [4,2]) == [2,4]
+assert intersection([4,2,1], [1,2,4,6]) == [1,2,4]
+assert intersection([0,1,2], [10,11]) == []
+
+a = [ i for i in range(0, 50000) ]
+b = [ i for i in range(0, 50000) ]
+assert intersection(a, b) == [ i for i in range(0, 50000) ]
