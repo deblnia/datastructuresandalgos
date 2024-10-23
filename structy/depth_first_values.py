c9b49ed4e2fc2c6965a5fda3bb42529bb4ab648d
deblina <deblina@uchicago.edu>
2024-10-22 20:04:44 -0400

    recursive

--- structy/depth_first_values.py
+++ structy/depth_first_values.py
@@ -5,17 +5,27 @@ class Node:
         self.right = None 
 
 def depth_first_values(root):
-    if root is None: 
-        return []
+    # iterative 
+    # if root is None: 
+    #     return []
     
-    values = []
-    stack = [root]
-    while stack: 
-        curr = stack.pop()
-        values.append(curr.val)
+    # values = []
+    # stack = [root]
+    # while stack: 
+    #     curr = stack.pop()
+    #     values.append(curr.val)
 
-        if curr.right is not None: 
-            stack.append(curr.right)
-        if curr.left is not None: 
-            stack.append(curr.left)
-    return values 
+    #     if curr.right is not None: 
+    #         stack.append(curr.right)
+    #     if curr.left is not None: 
+    #         stack.append(curr.left)
+    # return values
+    
+    # recursive 
+    if not root:
+        return []
+
+    left_values = depth_first_values(root.left)
+    right_values = depth_first_values(root.right)
+
+    return [root.val, *left_values, *right_values]
