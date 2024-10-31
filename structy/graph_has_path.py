817029053f2cc38a1a6b00c706e23326306b37ca
deblina <deblina@uchicago.edu>
2024-10-31 13:11:36 -0400

    bfs

--- structy/graph_has_path.py
+++ structy/graph_has_path.py
@@ -1,4 +1,4 @@
-def has_path(graph, src, dst: 
+def has_path(graph, src, dst): 
     """
     This is the DFS implmenetation. 
     """
@@ -8,3 +8,16 @@ def has_path(graph, src, dst:
         if has_path(graph, neigh, dst):
             return True 
     return False
+
+from collections import deque 
+
+def has_path_bfs(graph, src, dst): 
+    q = deque([ src ])
+
+    while q: 
+        curr = q.popleft()
+    if curr == dst: 
+        return True 
+    for neigh in graph[curr]: 
+        q.append(neigh) 
+    return False 
