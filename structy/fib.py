5e5bf1367c3e65a6b22d184dc982a7a19ea414a7
deblina <deblina@uchicago.edu>
2024-11-05 22:15:01 -0500

    memoized version

--- structy/fib.py
+++ structy/fib.py
@@ -1,5 +1,15 @@
 def fib(n): 
     # brute force - this times out! 
-    if n == 0 or n == 1: 
-        return n
-    return fib(n - 1) + fib(n - 2)
+    # if n == 0 or n == 1:  
+    #     return n 
+    # return fib(n - 1) + fib(n - 2) 
+    memo = {}
+    return _fib(n, memo)
+
+def _fib(n, memo): 
+    if n==0 or n==1: 
+        return n 
+    if n in memo:
+        return memo[n]
+    memo[n] = _fib(n - 1, memo) + _fib(n - 2, memo)
+    return memo[n]
