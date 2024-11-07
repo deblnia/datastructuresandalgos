87c3604c81ac678e36642c199fda90684ab8d833
deblina <deblina@uchicago.edu>
2024-11-07 09:07:00 -0500

    momo

--- structy/sum_possible.py
+++ structy/sum_possible.py
@@ -1,10 +1,32 @@
 def sum_possible(amount, numbers):
-    # the brute force way to solve 
-    if amount == 0:
+    # the brute force way to solve - exponential (n^a) time
+    # if amount == 0:
+    #     return True 
+    # elif amount < 0:
+    #     return False 
+    # for num in numbers: 
+    #     if sum_possible(amount - num, numbers):
+    #         return True 
+    # return False
+
+    # here's the DP way - 
+    # note that we can start with the same solution that we had for BF 
+    # and just add in a memo 
+    return _sum_possible(amount, numbers, {})
+
+def _sum_possible(amount, numbers, memo):
+    if amount in memo: 
+        return memo[amount]
+        
+    if amount == 0: 
         return True 
-    elif amount < 0:
+    if amount < 0:
         return False 
+
     for num in numbers: 
-        if sum_possible(amount - num, numbers):
+        if _sum_possible(amount - num, numbers, memo):
+            memo[amount] = True
             return True 
-    return False
+
+    memo[amount] = False 
+    return False  
