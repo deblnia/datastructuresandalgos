80be92891f2b46eef01358f35879dc6235fa7f49
deblina <deblina@uchicago.edu>
2024-11-13 20:09:22 -0500

    valid first pass

--- leetcode/grind169/valid_parentheses.py
+++ leetcode/grind169/valid_parentheses.py
@@ -1,2 +1,17 @@
 class Solution:
-    def valid_parentheses(self, s: str)
+    def valid_parentheses(self, s: str): 
+        p = {
+            "(" : ")", 
+            "[" : "]", 
+            "{" : "}"
+        } 
+        stack = []
+        for i in s:
+            # opening bracket 
+            if i in p:
+                stack.append(i) 
+            # closing bracket 
+            elif i in p.values(): 
+               if not stack or p[stack.pop()] != i: 
+                   return False
+        return len(stack) == 0 
