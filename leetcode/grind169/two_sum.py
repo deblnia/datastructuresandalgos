7f1b60e79d9435fcd166404c3d0a3d677752090a
deblina <deblina@uchicago.edu>
2024-11-13 19:45:14 -0500

    two sum again

---
+++ leetcode/grind169/two_sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def two_sum(target:int, array:[List[int]]) -> (int, int):
+        d = {}
+        for i, v in enumerate(array):
+            if v not in d: 
+                d[target - v] = i
+            else:
+                return (d[v], i) 
