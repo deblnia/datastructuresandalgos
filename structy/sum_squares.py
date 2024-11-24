caf15f8d9b192f2ccf23809b47245f02ceb9862c
deblina <deblina@uchicago.edu>
2024-11-24 12:33:06 -0500

    sum_squares

--- structy/sum_squares.py
+++ structy/sum_squares.py
@@ -10,3 +10,23 @@ import math
 #     num_squares = 1 + summing_squares(n - square)  
 #     min_squares = min(min_squares, num_squares)
 #   return min_squares
+
+def summing_squares(n):
+    memo = {} 
+    return _summing_squares(n, memo)
+
+def _summing_squares(n, memo): 
+    if n in memo: 
+        return memo[n] 
+    if n==0: 
+       return 0
+    min_squares = float('inf')
+
+    for i in range(1, math.floor(math.sqrt(n)) + 1): 
+        square = i * i 
+        num_squares = 1 + summing_squares(n - square)
+        min_squares = min(min_squares, num_squares) 
+
+    memo[n] = min_squares
+    return min_squares    
+
