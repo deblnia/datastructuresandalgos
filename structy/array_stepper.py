446ca038b6f68ea63d3f58506c83530941aea463
deblina <deblina@uchicago.edu>
2024-11-27 16:51:25 -0500

    memoized array_stepper

--- structy/array_stepper.py
+++ structy/array_stepper.py
@@ -1,11 +1,29 @@
-def array_stepper(numbers):
-  return _array_stepper(numbers, 0)
+# Brute foce approach - this'll time out! 
+# def array_stepper(numbers):
+#   return _array_stepper(numbers, 0)
 
-def _array_stepper(numbers, i): 
+# def _array_stepper(numbers, i): 
+#   if i == len(numbers) - 1:
+#       return True 
+#   max_step = numbers[i] 
+#   for step in range(1, max_step + 1): 
+#     if _array_stepper(numbers, i + step):
+#       return True 
+#   return False 
+def array_stepper(numbers):
+  return _array_stepper(numbers, 0, {})
+
+def _array_stepper(numbers, i, memo): 
+  if i in memo:
+    return memo[i] 
+    
   if i == len(numbers) - 1:
       return True 
   max_step = numbers[i] 
   for step in range(1, max_step + 1): 
-    if _array_stepper(numbers, i + step):
+    if _array_stepper(numbers, i + step, memo):
+      memo[i] = True 
       return True 
+      
+  memo[i] = False 
   return False 
