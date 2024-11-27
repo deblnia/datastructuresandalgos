218b2d81685d13f530730ce92e8e673d35d108d2
deblina <deblina@uchicago.edu>
2024-11-27 17:01:07 -0500

    full recursive can concat

---
+++ structy/can_concat.py
@@ -0,0 +1,17 @@
+def can_concat(s, words):
+  return _can_concat(s, words, {})
+
+def _can_concat(s, words, memo):
+  if s in memo:
+    return memo[s]
+    
+  if s == '': 
+    return True 
+  for word in words: 
+    if s.startswith(word):
+      suffix = s[len(word):] 
+      if can_concat(suffix, words) == True:
+        memo[s] = True 
+        return True
+  memo[s] = False 
+  return False 
