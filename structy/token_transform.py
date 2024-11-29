bac770028cf49bd4b248ea95001d35f933109829
deblina <deblina@uchicago.edu>
2024-11-29 10:06:21 -0500

    token_transform

---
+++ structy/token_transform.py
@@ -0,0 +1,21 @@
+def token_transform(s, tokens):
+  output = []
+  i = 0
+  j = 1
+  while i < len(s):
+    if s[i] != '$':
+      output += s[i]
+      i += 1
+      j = i + 1
+    elif s[j] != '$':
+      j += 1
+    else:
+      key = s[i:j + 1]
+      value = tokens[key]
+      evaluated_value = token_transform(value, tokens)
+      tokens[key] = evaluated_value
+      output.append(evaluated_value)
+      i = j + 1
+      j = i + 1
+  
+  return ''.join(output)
