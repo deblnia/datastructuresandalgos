d3a19c01da13155a1e620c68ca7f1e59a5121810
deblina <deblina@uchicago.edu>
2024-12-08 23:16:41 -0500

    ok

--- design-gurus/sqrt.py
+++ design-gurus/sqrt.py
@@ -3,4 +3,21 @@ import math
 class Solution: 
     def mySqrt(self, x: int) -> int:
         # I think the trick here is python's crazy default rounding  
-        return math.floor(math.sqrt(x)) 
+        # return math.floor(math.sqrt(x)) 
+        # okay no they actually wanted to do it 
+        if x < 2: 
+            return x # 0 or 1 case 
+        
+        l, r = 2, x // 2 
+        mid = 0 
+        num = 0 
+        while l <= r: 
+            mid = l + (r - l) # middle element 
+            num = mid * mid 
+            if num > x: 
+                r = mid - 1 
+            elif num < x: 
+                l = mid + 1 
+            else: 
+                return mid 
+        return right 
