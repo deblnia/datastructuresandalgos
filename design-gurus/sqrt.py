82e7e94101ea06e3faaca2d2457215b4240d5d84
deblina <deblina@uchicago.edu>
2024-12-08 23:13:23 -0500

    warmups done

--- design-gurus/sqrt.py
+++ design-gurus/sqrt.py
@@ -1,5 +1,6 @@
 import math 
 
 class Solution: 
-    def mySqrt(self, x: int) -> int: 
-        return 0 
+    def mySqrt(self, x: int) -> int:
+        # I think the trick here is python's crazy default rounding  
+        return math.floor(math.sqrt(x)) 
