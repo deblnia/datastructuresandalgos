85fd995e338c0191357a5b7335b23cdd7882964c
deblina <deblina@uchicago.edu>
2024-12-24 17:26:31 -0500

    not sure if i understand but ok

--- design-gurus/squaring_sorted.py
+++ design-gurus/squaring_sorted.py
@@ -1,13 +1,30 @@
 def makeSquares(arr):
-    # hacky built-ins 
-    squares = sorted([x**2 for x in arr])
-    # squares.sort() # modifies in place 
+    # # hacky built-ins, this is o(n*log(n)) time 
+    # squares = sorted([x**2 for x in arr])
+    # # squares.sort() # modifies in place 
+    # return squares
+
+    # what they actually want is the o(n) two pointer solution 
+    n = len(arr)
+    squares = [0 for _ in range(n)]
+
+    highestSquareIdx = n - 1
+    l, r = 0, n - 1
+
+    while l <= r: 
+        ls = arr[l] ** 2 
+        rs = arr[r] ** 2 
+
+        if ls >= rs: 
+            squares[highestSquareIdx] = ls
+            l += 1 
+        else: 
+            squares[highestSquareIdx] = rs 
+            r -= 1
+        highestSquareIdx -= 1
     return squares
+        
 
 
-
-
-
-print(makeSquares([-2, -1, 0, 2, 3]))
-# assert makeSquares([-2, -1, 0, 2, 3]) == [0, 1, 4, 4, 9]
-# assert makeSquares([-3, -1, 0, 1, 2]) == [0, 1, 1, 4, 9]
+assert makeSquares([-2, -1, 0, 2, 3]) == [0, 1, 4, 4, 9]
+assert makeSquares([-3, -1, 0, 1, 2]) == [0, 1, 1, 4, 9]
