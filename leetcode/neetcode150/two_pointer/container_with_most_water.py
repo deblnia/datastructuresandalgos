c2c04704da00c75890cf9496566a1ffd756543a7
Deblina Mukherjee <mynameisdeb.m@gmail.com>
2025-02-20 21:13:05 -0500

    two pointers getting clearer

---
+++ leetcode/neetcode150/two_pointer/container_with_most_water.py
@@ -0,0 +1,16 @@
+
+def maxArea(heights: list[int]) -> int:
+    l, r = 0, len(heights) - 1
+    max_area = 0
+    while l < r:
+        area = (r - l) * min(heights[l], heights[r])
+        max_area = max(max_area, area)
+        if heights[l] < heights[r]:
+            l += 1
+        else:
+            r -= 1
+    return max_area
+
+
+assert maxArea([1,7,2,5,4,7,3,6]) == 36
+assert maxArea([2,2,2]) == 4
